cba35fa41fc1a3ef499fce3969c39a5d1193ff0b
MissChenduo <zdt5500@autuni.ac.nz>
2022-09-16 22:29:38 +1200

    Updated
    Updated

---
+++ Portfolio/portfolio/main.js
@@ -0,0 +1,27 @@
+
+function showContent(id) {
+  $("div#home")
+  .removeClass(id == "home" ? "hide" : "show")
+  .addClass(id == "home" ? "show" : "hide");
+    $("div#about")
+      .removeClass(id == "about" ? "hide" : "show")
+      .addClass(id == "about" ? "show" : "hide");
+    $("div#research")
+      .removeClass(id == "research" ? "hide" : "show")
+      .addClass(id == "research" ? "show" : "hide");
+    $("div#planning")
+      .removeClass(id == "planning" ? "hide" : "show")
+      .addClass(id == "planning" ? "show" : "hide");
+    $("div#communication")
+      .removeClass(id == "communication" ? "hide" : "show")
+      .addClass(id == "communication" ? "show" : "hide");
+    $("div#devandqa")
+      .removeClass(id == "devandqa" ? "hide" : "show")
+      .addClass(id == "devandqa" ? "show" : "hide");
+      $("div#Presentation")
+      .removeClass(id == "Presentation" ? "hide" : "show")
+      .addClass(id == "Presentation" ? "show" : "hide");
+      $("div#Team_Workbooks")
+      .removeClass(id == "Team_Workbooks" ? "hide" : "show")
+      .addClass(id == "Team_Workbooks" ? "show" : "hide");
+  }
