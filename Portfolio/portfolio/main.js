5d9961dde36a14f47320d10b9272824d4b1919a9
Vinson Beduya <107016542+tzq8947@users.noreply.github.com>
2022-10-10 15:22:26 +1300

    Added underline for selected navigation. (#1)
    Co-authored-by: Vinson Beduya <vinsonemb.151994@gmail.com>

--- Portfolio/portfolio/main.js
+++ Portfolio/portfolio/main.js
@@ -1,32 +1,36 @@
+const DIV_IDS = ["home", "about", "research", "planning", "communication", "devandqa", "Presentation", "Team_Workbooks", "Reflection"]
+const APPENDIX_DIV_IDS = ["Presentation", "Team_Workbooks", "Reflection"]
+const DROPDOWN_ID = "navbarDropdownPortfolio"
 
-function showContent(id) {
-  $("div#home")
-  .removeClass(id == "home" ? "hide" : "show")
-  .addClass(id == "home" ? "show" : "hide");
-    $("div#about")
-      .removeClass(id == "about" ? "hide" : "show")
-      .addClass(id == "about" ? "show" : "hide");
-    $("div#research")
-      .removeClass(id == "research" ? "hide" : "show")
-      .addClass(id == "research" ? "show" : "hide");
-    $("div#planning")
-      .removeClass(id == "planning" ? "hide" : "show")
-      .addClass(id == "planning" ? "show" : "hide");
-    $("div#communication")
-      .removeClass(id == "communication" ? "hide" : "show")
-      .addClass(id == "communication" ? "show" : "hide");
-    $("div#devandqa")
-      .removeClass(id == "devandqa" ? "hide" : "show")
-      .addClass(id == "devandqa" ? "show" : "hide");
-      $("div#Presentation")
-      .removeClass(id == "Presentation" ? "hide" : "show")
-      .addClass(id == "Presentation" ? "show" : "hide");
-      $("div#Team_Workbooks")
-      .removeClass(id == "Team_Workbooks" ? "hide" : "show")
-      .addClass(id == "Team_Workbooks" ? "show" : "hide");
-      $("div#Reflection")
-      .removeClass(id == "Reflection" ? "hide" : "show")
-      .addClass(id == "Reflection" ? "show" : "hide");
-  }
+const showContent = (id) => {
+  DIV_IDS.forEach(divId => {
+    const divIdQuery = $(`div#${divId}`)
+    const navLinkQuery = $(`a#nav-item-${divId}`)
+    const dropdownQuery = $(`a#${DROPDOWN_ID}`)
+    if (id == divId) {
+      divIdQuery
+        .addClass("show")
+        .removeClass("hide")
 
-  
+      // Add Nav Item Underline
+      navLinkQuery.addClass("underline")
+
+      // Add Dropdown Appendix Underline
+      if (APPENDIX_DIV_IDS.includes(divId)) {
+        dropdownQuery.addClass("underline")
+      }
+    } else {
+      divIdQuery
+        .addClass("hide")
+        .removeClass("show")
+
+      // Remove Nav Item Underline
+      navLinkQuery.removeClass("underline")
+
+      // Remove Dropdown Appendix Underline
+      if (!APPENDIX_DIV_IDS.includes(divId)) {
+        dropdownQuery.removeClass("underline")
+      }
+    }
+  })
+}
